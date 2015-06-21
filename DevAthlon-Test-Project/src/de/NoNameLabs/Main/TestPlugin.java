cf8baac47031b98b7e20e27ade7c1c771caeaa5a
BuildTools <unconfigured@null.spigotmc.org>
2015-06-21 21:41:21 +0200

    UPDATEs

--- DevAthlon-Test-Project/src/de/NoNameLabs/Main/TestPlugin.java
+++ DevAthlon-Test-Project/src/de/NoNameLabs/Main/TestPlugin.java
@@ -1,21 +1,28 @@
 package de.NoNameLabs.Main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class TestPlugin extends JavaPlugin {
+	
+	public static TestPlugin INSTANCE;
 	
 	Logger log = this.getLogger();
 	
 	List<Player> player_music = new ArrayList<Player>();
+	Map<Player, Effect> player_effects = new HashMap<Player, Effect>();
 	
 	Note[][] music = {
 		{Note.D},		{Note.F},		{Note.D2},		{}, 	{},		{},		{Note.D},		{Note.F},
@@ -33,11 +40,13 @@ public class TestPlugin extends JavaPlugin {
 	};
 	
     public void onEnable(){ 
+    	INSTANCE = this;
         log.info("Plugin gestartet");
         
         getServer().getPluginManager().registerEvents(new TestListener(), this);
         
         music();
+        effects();
     }
      
     public void onDisable(){ 
@@ -94,5 +103,17 @@ public class TestPlugin extends JavaPlugin {
     			}
     		}
     	}, 0L, 5L);
+    }
+    
+    public void effects() {
+    	this.getServer().getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
+    		public void run() {
+    			synchronized (player_effects) {
+    				for (Player p: player_effects.keySet()) {
+        				p.getWorld().spigot().playEffect(p.getLocation(), player_effects.get(p), 0, 0, (float) (Math.random() / 4), 1, (float) (Math.random() / 4), 0.2f, 5, 1);
+        			}
+    			}
+    		}
+    	}, 0L, 2L);
     }
 }
